455484ba6fd41fcdeba1e7b703941944416497a8
stevenpslade <stevenpslade@gmail.com>
2016-04-26 10:45:00 -0700

    registering asset in plugin.rb file to see if changes will take effect

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: Discourse Alternate Profile Layout
+# about: Alter Profile Layout template
+# version: 1.0
+# authors: Steven Slade
+# url: https://github.com/stevenpslade/discourse-alt-profile-layout-new
+
+register_asset "javascripts/discourse/templates/user/user.hbs"
